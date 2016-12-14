7764e5d6be51ce14809f1b524ec109888c05d5b5
ravenusmc <mcuddy77@gmail.com>
2016-12-14 17:05:25 -0500

    finished project 2

--- one/index.js
+++ one/index.js
@@ -1,4 +1,11 @@
+//Code for Project One
 function buttonpush(){
-  var value = document.getElementById("input-area").value;
+  let value = document.getElementById("input-area").value;
   alert(value);
 };
+
+//Code for Project 2 
+function changeHeading(){
+  let heading = document.getElementById('input-area-two').value;
+  document.getElementById("headingTwo").innerHTML = heading;
+}
