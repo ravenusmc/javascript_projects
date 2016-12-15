bab8cc137851bdc67917919dcaaf233b848763fb
ravenusmc <mcuddy77@gmail.com>
2016-12-15 17:13:36 -0500

    added a fire button

--- two/index.js
+++ two/index.js
@@ -1,14 +1,53 @@
-const hit = true;
 
-function test(hit){
+//variables
+let hit = 0;
+let miss = 0;
+
+//This function will let the user know their hit count
+function hits(hit){
+  document.getElementById("hitArea").innerHTML = hit;
+}
+
+//This function will let the user know their miss count
+function misses(miss){
+  document.getElementById('missArea').innerHTML = miss
+}
+
+//This function will determine if a shell is a hit or miss.
+function fireShell(){
+
+  let fire = true;
+
+  let num = Math.floor(Math.random()*20);
+
+    if (num > 5){
+      fire = false;
+      return fire;
+    }else{
+      fire = true;
+      return fire;
+    }
+}
+
+//This function will determine if the user hit a space. 
+function attack(){
+
   let target = document.getElementById('one');
   
-  if (hit){
-    alert("Hit!");
+  fire = fireShell();
+  
+  if (fire){
+    target.style.color = 'green';
+    hit += 1;
+    hits(hit);
   }else {
-    alert("Miss");
+    target.style.color = 'red';
+    miss += 1;
+    misses(miss);
   }
+
 }
+
 
 //Code for Directions appear and disappearing 
 const toggleList = document.getElementById('toggleList'); //Getting the button 
@@ -21,7 +60,7 @@ toggleList.addEventListener('click', () => {
     toggleList.textContent = 'Push to See Directions';
     listDiv.style.display = 'none'; 
   }else{
-    toggleList.textContent = 'Hide List';
+    toggleList.textContent = 'Hide Directions';
     listDiv.style.display = 'block';
   }
 });
