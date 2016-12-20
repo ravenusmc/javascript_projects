afbcb544aba8dec8df89d85d94e068bae5457222
ravenusmc <mcuddy77@gmail.com>
2016-12-20 17:34:52 -0500

    added comment

--- three/three.js
+++ three/three.js
@@ -1,5 +1,7 @@
+
+//This function will change the the first image on the page when it is clicked.
 function changeOne(){
 
   document.getElementById("imageOne").src = "http://phandroid.s3.amazonaws.com/wp-content/uploads/2014/05/surface-planets.jpg";
-  
+
 }
