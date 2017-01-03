56b0c8a3c81cdd8f9425d4b560fc8b4984314164
ravenusmc <mcuddy77@gmail.com>
2017-01-03 17:03:57 -0500

    minor changes, added comments

--- four/four.js
+++ four/four.js
@@ -1,13 +1,20 @@
+//This function will be repeated over and over again. Partly for me to really get use to working with 
+//Javascript. The function will change the image, heading and paragraph in the first image. All
+//ther functions will be a replica of this one. 
 function changeOne(){
 
   let target = document.getElementById('one');
 
   if (target.style.color == 'black'){
     document.getElementById('one').src = "https://source.unsplash.com/it-_C9yfRIM/255x200";
-    document.getElementById('one-heading').
+    let heading = "Have the courage to open the door";
+    document.getElementById("one-heading").innerHTML = heading;
     target.style.color = 'blue';
+
   }else{
     document.getElementById('one').src = "https://source.unsplash.com/_FjegPI89aU/255x200";
+    let heading = "A lonely House...";
+    document.getElementById('one-heading').innerHTML = heading;
     target.style.color = 'black';
   }
 }
